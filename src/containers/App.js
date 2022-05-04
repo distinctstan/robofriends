6e130c5b582992b5c99f8d4028e4807e71a80db2
Mac <mac@Macs-MacBook-Pro.local>
2022-05-04 03:33:00 +0100

    Second commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import { SearchBox } from "../components/SearchBox";
 import { Component } from 'react';
 import './App.css';
 import { Scroll } from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 
 class App extends Component {
 
@@ -45,7 +46,9 @@ class App extends Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             )
